99feb8ba9be9ba4ce7325cf967fe29778f919c99
StasMykhailov <s.m.mykhailov@gmail.com>
2016-06-20 20:37:19 +0300

    part of task 2

--- Control-tasks-20.06.2016/task_2.php
+++ Control-tasks-20.06.2016/task_2.php
@@ -1,7 +1,22 @@
+
+<form action="" method="post">
+    <p><b>Введите Вашу ссылку:</b></p>
+    <p><input type="text" name="link"></p>
+    <p><input type="submit" value="Найти картинки!"></p>
+</form>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Stas
- * Date: 20.06.2016
- * Time: 19:41
- */
+if (isset($_POST['link']) && !empty($_POST['link'])) {
+
+    $link = file_get_contents($_POST['link']);
+    preg_match_all("/<img src='(.*)'.*?>/", $link, $results);
+    var_dump($results);
+    foreach ($results[1] as $image){?>
+    <p><a href="<?php echo $image?>"</p>
+
+    <?php
+    }
+}
+
+?>
+
